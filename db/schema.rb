92f8b0d9354a0b4bca558f928696d78a56d473be
Javier Perez <perez.javi.a@gmail.com>
2017-05-11 19:40:40 -0400

    Adding name to applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511231304) do
+ActiveRecord::Schema.define(version: 20170511234004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170511231304) do
   create_table "instances", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["name"], name: "index_instances_on_name"
   end
 
   create_table "logs", force: :cascade do |t|
