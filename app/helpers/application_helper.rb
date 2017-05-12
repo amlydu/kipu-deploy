3eb9c36b9b1163160483101369576ea65b00795d
Amy Dusek <amlydu@umich.edu>
2017-05-12 12:49:57 -0400

    Update views to be object specific

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def instance_box_color_class(status = nil)
+    # color classes = panel-heading (blue), panel-green, panel-red, panel-yellow
+    case status
+    when nil
+      'panel-yellow'
+    when 'error'
+      'panel-red'
+    when 'success'
+      'panel-green'
+    else
+      'panel-heading' #blue
+    end
+  end
+
+  def progress_bar_color_class(progress_status = nil)
+    # progress classes: progress-bar-success (green), -info is blue, -warning (orange), -danger (red)
+    case progress_status
+    when nil
+      'progress-bar-warning'
+    when 'error'
+      'progress-bar-danger' #orange
+    when 'success'
+      'progress-bar-success' #green
+    else
+      'progress-bar-info' #blue
+    end
+  end
 end
