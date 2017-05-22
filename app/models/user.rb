72e919fabcd723c0b06182f2eb643e8bb7544c2a
Javier Perez <perez.javi.a@gmail.com>
2017-05-22 11:58:19 -0400

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_format_of :email, with: /\@kipusystems\.com/,
+                              message: "You need a kipusystems.com email!"
 end
