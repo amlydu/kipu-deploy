a45f1ec857c9ad3925b1bb4970067670a8bb92fd
Javier Perez <perez.javi.a@gmail.com>
2017-05-22 15:21:32 -0400

    Refactoring whitespace

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,13 +5,19 @@ class Log < ApplicationRecord
 
   def broadcast_data
     instance = self.release.instance
-    ActionCable.server.broadcast "instance_#{instance.id}_channel", data: render_data(instance)
-    ActionCable.server.broadcast "instance__channel", data: {instance_id: instance.id, div: render_data(instance) }
+    ActionCable.server.broadcast "instance_#{instance.id}_channel",
+                                 data: render_data(instance)
+    ActionCable.server.broadcast "instance__channel",
+                                 data: {
+                                   instance_id: instance.id,
+                                   div: render_data(instance) }
   end
 
   private
 
   def render_data(inst)
-    ApplicationController.renderer.render(partial: 'instances/instance_box', locals: { instance: inst })
+    ApplicationController.renderer.render(
+      partial: 'instances/instance_box', locals: { instance: inst }
+    )
   end
 end
